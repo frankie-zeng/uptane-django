3046305b25f19f41f5ce65c4791b42a770f94284
Frankie Zeng <frankie.zeng@nxp.com>
2020-04-13 09:19:40 +0800

    add inventory db model

---
+++ uptane_django/inventorydb/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Vehicle,Ecu
+# Register your models here.
+
+admin.site.register(Vehicle)
+admin.site.register(Ecu)
--- uptane_django/uptane_django/settings.py
+++ uptane_django/uptane_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventorydb.apps.InventorydbConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
